f1f7eb7574b37dfab578daef632f2123ca0d0213
Fredy Sánchez <00082817@uca.edu.sv>
2020-05-25 18:21:49 -0600

    Estructura inicial de proyecto

---
+++ src/main/java/com/uca/capas/Parcial02Capas00082817Application.java
@@ -0,0 +1,13 @@
+package com.uca.capas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Parcial02Capas00082817Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Parcial02Capas00082817Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/uca/capas/Parcial02Capas00082817ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uca.capas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Parcial02Capas00082817ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
